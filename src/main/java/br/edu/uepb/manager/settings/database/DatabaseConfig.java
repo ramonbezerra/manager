16081027b93fc9b0ac1ecc6628a33c9c6576c0a5
ramonbezerra <ramon.bezerra@outlook.com>
2022-03-30 20:23:26 -0300

    add cors config and modify database

--- src/main/java/br/edu/uepb/manager/settings/database/DatabaseConfig.java
+++ src/main/java/br/edu/uepb/manager/settings/database/DatabaseConfig.java
@@ -5,16 +5,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
-@Configuration
+// @Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+  // @Value("${spring.datasource.url}")
+  // private String dbUrl;
 
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
+  // @Bean
+  // public DataSource dataSource() {
+  //     HikariConfig config = new HikariConfig();
+  //     config.setJdbcUrl(dbUrl);
+  //     return new HikariDataSource(config);
+  // }
 }
